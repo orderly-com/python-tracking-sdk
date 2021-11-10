9e86988ca74c2689d3e676973f87b293f1cf8876
ray.yang <ray.yang@ezorderly.com>
2021-11-10 15:42:48 +0800

    bf

--- src/orderly_tracking/tracking.py
+++ src/orderly_tracking/tracking.py
@@ -1,8 +1,8 @@
-import json
 import requests
 from typing import Optional
 from . import settings
-def record_event(self, msg: str, place: str, pt: str, title: str,
+
+def record_event(msg: str, place: str, pt: str, title: str,
     language: str='zh-tw', cid: Optional[str]=None, decode_format: str='UTF-8',
     sd: str='24-bit', sr: str='1920x1080', did: str='tl', view_port_size: str='1905x887'):
 
@@ -19,4 +19,8 @@ def record_event(self, msg: str, place: str, pt: str, title: str,
         'did': did,
         'vp': view_port_size
     }
+    record_raw_event(payload)
+
+
+def record_raw_event(payload): # payload must match the standard format
     requests.get(settings.RELAY_URL+'/api/1/1', params=payload)
