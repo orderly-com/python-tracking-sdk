dc009dcae6ab8baeb4c05cd3ceabd398bcfc2f0e
ray.yang <ray.yang@ezorderly.com>
2021-11-10 12:07:31 +0800

    Tracker

---
+++ src/orderly_tracking/trackers.py
@@ -0,0 +1,33 @@
+import json
+
+from typing import Optional
+from aiokafka import AIOKafkaProducer
+
+import settings
+
+class Tracker:
+    def __init__(self, **producer_params):
+        self.producer = AIOKafkaProducer(bootstrap_servers='localhost:9092', **producer_params)
+
+    def connect_target(self):
+        pass
+
+    def record_event(self, msg: str, place: str, pt: str, title: str,
+        language: str='zh-tw', cid: Optional[str]=None, decode_format: str='UTF-8',
+        sd: str='24-bit', sr: str='1920x1080', did: str='tl', view_port_size: str='1905x887'):
+
+        payload = {
+            'v': msg,
+            'cid': cid,
+            'de': decode_format,
+            'at': place,
+            'ul': language,
+            'pt': pt,
+            'sd': sd,
+            'sr': sr,
+            'tl': title,
+            'did': did,
+            'vp': view_port_size
+        }
+
+        self.producer.send(settings.KAFKA_TOPIC, json.dump(payload), partition=0)
