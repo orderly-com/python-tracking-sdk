0743c39dd9d3ef7e4bb98fae79bdca0d72ce1166
ray.yang <ray.yang@ezorderly.com>
2021-11-11 18:53:48 +0800

    AVALIBLE_ACTIONS

--- src/orderly_tracking/settings.py
+++ src/orderly_tracking/settings.py
@@ -1,2 +1,3 @@
 KAFKA_TOPIC = 'tracking'
 RELAY_URL = 'http://localhost:3000'
+CEREM_URL = 'http://localhost:1218'
--- src/orderly_tracking/tracking.py
+++ src/orderly_tracking/tracking.py
@@ -1,26 +1,38 @@
 import requests
+import uuid
 from typing import Optional
 from . import settings
 
-def record_event(msg: str, place: str, pt: str, title: str,
+ACTION_VIEW = 'view'
+ACTION_CLICK = 'click'
+
+AVALIBLE_ACTIONS = [ACTION_VIEW, ACTION_CLICK]
+
+class ActionTypeNotAllowed(Exception):
+    pass
+
+def record_event(version: str, action: str, url: str, title: str,
     language: str='zh-tw', cid: Optional[str]=None, decode_format: str='UTF-8',
     sd: str='24-bit', sr: str='1920x1080', did: str='tl', view_port_size: str='1905x887'):
 
+    if action not in AVALIBLE_ACTIONS:
+        raise ActionTypeNotAllowed(f'{action} is not a valid action type, use tracking.ACTION_VIEW instead. Check tracking.AVALIBLE_ACTIONS for choices')
+
+    if cid is None:
+        cid = uuid.uuid4().hex[:16].lower()
+
     payload = {
-        'v': msg,
+        'v': version,
         'cid': cid,
         'de': decode_format,
-        'at': place,
+        'at': action,
         'ul': language,
-        'pt': pt,
+        'pt': url,
         'sd': sd,
         'sr': sr,
         'tl': title,
         'did': did,
         'vp': view_port_size
     }
-    record_raw_event(payload)
-
-
-def record_raw_event(payload): # payload must match the standard format
     requests.get(settings.RELAY_URL+'/api/1/1', params=payload)
+    return cid
