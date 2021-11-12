f4dbdc594579589763ca43aeb32b53e580fad273
kai <kai@ezorderly.com>
2021-11-12 19:36:49 +0800

    add ds_id change team_code to tc modify api path

--- src/orderly_tracking/tracking.py
+++ src/orderly_tracking/tracking.py
@@ -3,34 +3,35 @@ from typing import Optional
 
 
 class Tracker:
-    def __init__(self, team_code, relay_url, cerem_url) -> None:
+    def __init__(self, team_code, ds_id, relay_url, cerem_url) -> None:
         self.team_code = team_code
+        self.ds_id = ds_id
         self.relay_url = relay_url
         self.cerem_url = cerem_url
 
     def click_event(self, version: str, url: str, title: str, target: str,
-            language: str='zh-tw', cid: Optional[str]=None, decode_format: str='',
-            sd: str='', sr: str='', did: str='', view_port_size: str=''
-    ):
+                    language: str = 'zh-tw', cid: Optional[str] = None, decode_format: str = '',
+                    sd: str = '', sr: str = '', did: str = '', view_port_size: str = ''
+                    ):
         self._record_event(
             version=version, action='click', url=url, title=title, target=target,
-            language=language, cid=cid, decode_format=decode_format,sd=sd,
+            language=language, cid=cid, decode_format=decode_format, sd=sd,
             sr=sr, did=did, view_port_size=view_port_size
         )
 
     def view_event(self, version: str, url: str, title: str, target: str,
-            language: str='zh-tw', cid: Optional[str]=None, decode_format: str='',
-            sd: str='', sr: str='', did: str='', view_port_size: str=''
-    ):
+                   language: str = 'zh-tw', cid: Optional[str] = None, decode_format: str = '',
+                   sd: str = '', sr: str = '', did: str = '', view_port_size: str = ''
+                   ):
         self._record_event(
             version=version, action='view', url=url, title=title, target=target,
-            language=language, cid=cid, decode_format=decode_format,sd=sd,
+            language=language, cid=cid, decode_format=decode_format, sd=sd,
             sr=sr, did=did, view_port_size=view_port_size
         )
 
     def _record_event(self, version: str, action: str, url: str, title: str, target: str,
-        language: str='zh-tw', cid: Optional[str]=None, decode_format: str='',
-        sd: str='', sr: str='', did: str='', view_port_size: str=''):
+                      language: str = 'zh-tw', cid: Optional[str] = None, decode_format: str = '',
+                      sd: str = '', sr: str = '', did: str = '', view_port_size: str = ''):
 
         if cid is None:
             try:
@@ -40,7 +41,7 @@ class Tracker:
                 return
 
         payload = {
-            'team_code': self.team_code,
+            'tc': self.team_code,
             'v': version,
             'cid': cid,
             'tg': target,
@@ -54,5 +55,5 @@ class Tracker:
             'did': did,
             'vp': view_port_size
         }
-        requests.get(self.relay_url + '/api/1/1', params=payload)
+        requests.get(self.relay_url + '/api/'+ self.ds_id +'/tracking/', params=payload)
         return cid
